626267f1f8b64b4ec593124b95f30e37b281425d
akikaaa <yuchunlong111@gmail.com>
2023-05-15 09:37:05 +0800

    update

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 accelerate launch finetune.py \
-    --base_model 'decapoda-research/llama-7b-hf' \
+    --base_model 'llama-7b-hf' \
     --data_path 'yahma/alpaca-cleaned' \
     --output_dir './lora-alpaca'
